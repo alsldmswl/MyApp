5ffcb2d0a36ce814e0b9c7197fbe5d2770298ede
alsldmswl <jej1452@gmail.com>
2023-05-01 20:34:10 +0900

    Add models

---
+++ RickAndMorty/RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMorty
+//
+//  Created by eun-ji on 2023/05/01.
+//
+
+import Foundation
+
+enum RMCharaterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
