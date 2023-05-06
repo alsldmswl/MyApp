fd74764cf004420b5dd58a03091bb7f1b9c09b0d
alsldmswl <jej1452@gmail.com>
2023-05-06 20:02:11 +0900

    Tap Setting options

--- RickAndMorty/RickAndMorty/Controllers/Core/RMSettingViewController.swift
+++ RickAndMorty/RickAndMorty/Controllers/Core/RMSettingViewController.swift
@@ -4,15 +4,13 @@
 //
 //  Created by eun-ji on 2023/05/01.
 //
-
+import SafariServices
 import SwiftUI
 import UIKit
 
 final class RMSettingViewController: UIViewController {
 
-    private let settingsSwiftUIController = UIHostingController(rootView: RMSettingsView(viewModel: RMSettingsViewViewModel(cellViewModels: RMSettingsOption.allCases.compactMap({
-        return RMSettingsCellViewModel(type: $0)
-    }))))
+    private var settingsSwiftUIController: UIHostingController<RMSettingsView>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +20,11 @@ final class RMSettingViewController: UIViewController {
     }
     
     private func addSwiftUIController() {
+       let settingsSwiftUIController = UIHostingController(rootView: RMSettingsView(viewModel: RMSettingsViewViewModel(cellViewModels: RMSettingsOption.allCases.compactMap({
+           return RMSettingsCellViewModel(type: $0) {[weak self] option in
+               self?.handleTap(option: option)
+           }
+        }))))
         addChild(settingsSwiftUIController)
         settingsSwiftUIController.didMove(toParent: self)
         
@@ -34,7 +37,19 @@ final class RMSettingViewController: UIViewController {
             settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
             settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
+        self.settingsSwiftUIController = settingsSwiftUIController
     }
-
- 
+    private func handleTap(option: RMSettingsOption) {
+        guard Thread.current.isMainThread else {
+            return
+        }
+        
+        if let url = option.targetUrl {
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+        } else if option == .rateApp {
+            
+        }
+    }
+        
 }
