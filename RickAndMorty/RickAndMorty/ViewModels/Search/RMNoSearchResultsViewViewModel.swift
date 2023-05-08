6a8496538ee72dab8941163d69434a130cfeb490
alsldmswl <jej1452@gmail.com>
2023-05-08 13:52:02 +0900

    Added no search results view

---
+++ RickAndMorty/RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RMNoSearchResultsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by eun-ji on 2023/05/08.
+//
+
+import UIKit
+
+struct RMNoSearchResultsViewViewModel {
+    
+    let title = "No Results"
+    let image = UIImage(systemName: "magnifyingglass.circle")
+}
